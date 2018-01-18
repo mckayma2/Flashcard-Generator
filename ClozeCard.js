e807221bce1ed5feab50468e90e2b307d0938094
Mackay <mario.mackay@marineharvest.com>
2018-01-18 18:09:00 -0500

    first Upload

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,1 +1,31 @@
 
+var inquirer = require('inquirer');
+var ClozeCard = function(text, cloze){
+
+this.text = text;
+this.cloze = cloze;
+this.partial = "..." + this.cloze; //only the cloze-deleted portion of the text.
+this.fullText = this.text + " " + this.cloze; //fullText
+this.Arr = [this.text, this.cloze, this.partial, this.fullText];
+}
+
+var test = new ClozeCard("Mario", "is a student at UM Coding Boot Camp")
+inquirer
+  .prompt([
+    {
+      type: 'list',
+      name: test,
+      message: test.partial,
+      choices: [test.text, "test", "steve"],
+      filter: function(val) {
+        
+        return val.toLowerCase();
+
+      	}	
+
+    }
+   
+  ])
+  .then(answers => {
+    console.log(JSON.stringify(answers, null, '  '));
+  });
